524a6d8ec41a9ea057c461a706ac3b7c38b444f4
Edilson Daniel de Souza <edilsondesouzahonda@gmail.com>
2021-09-18 13:47:07 -0300

    Utilização do Factory

---
+++ connection_factory.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import MySQLdb
+
+
+class Connection_Factory(object):
+    def get_connection(self):
+        connection = MySQLdb.connect(
+            host='localhost',
+            user='root',
+            passwd='',
+            db='alura')
+
+        return connection
