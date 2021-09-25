16027f312ed821d773775217b7355ea8664254fa
Edilson Daniel de Souza <edilsondesouzahonda@gmail.com>
2021-09-25 09:31:23 -0300

    Utilização do Visitor

--- operacoes.py
+++ operacoes.py
@@ -1,12 +1,24 @@
-class Substracao(object):
+class Subtracao(object):
 
     def __init__(self, expressao_esquerda, expressao_direita):
-        self.__expressao_esquerda = expressao_esquerda 
+        self.__expressao_esquerda = expressao_esquerda
         self.__expressao_direita = expressao_direita
 
     def avalia(self):
         return (self.__expressao_esquerda.avalia()
-            - self.__expressao_direita.avalia())
+                - self.__expressao_direita.avalia())
+
+    @property
+    def expressao_esquerda(self):
+        return self.__expressao_esquerda
+
+    @property
+    def expressao_direita(self):
+        return self.__expressao_direita
+
+    def aceita(self, visitor):
+        visitor.visita_subtracao(self)
+
 
 class Soma(object):
 
@@ -16,7 +28,19 @@ class Soma(object):
 
     def avalia(self):
         return (self.__expressao_esquerda.avalia()
-            + self.__expressao_direita.avalia())
+                + self.__expressao_direita.avalia())
+
+    def aceita(self, visitor):
+        visitor.visita_soma(self)
+
+    @property
+    def expressao_esquerda(self):
+        return self.__expressao_esquerda
+
+    @property
+    def expressao_direita(self):
+        return self.__expressao_direita
+
 
 class Numero(object):
 
@@ -26,11 +50,16 @@ class Numero(object):
     def avalia(self):
         return self.__numero
 
-if __name__ == '__main__':
+    def aceita(self, visitor):
+        visitor.visita_numero(self)
 
-    expressao_esquerda = Substracao(Numero(10), Numero(5))
+
+if __name__ == '__main__':
+    from impressao import Impressora
+
+    expressao_esquerda = Subtracao(Numero(10), Numero(5))
     expressao_direita = Soma(Numero(2), Numero(10))
     expressao_conta = Soma(expressao_esquerda, expressao_direita)
 
-    resultado = expressao_conta.avalia()
-    print(resultado)
+    visitor = Impressora()
+    expressao_conta.aceita(visitor)
